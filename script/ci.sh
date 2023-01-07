a3c3988229b2f398336466b776d56148ccde1bde
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-01-07 12:02:22 +0900

    fix

--- script/ci.sh
+++ script/ci.sh
@@ -6,6 +6,7 @@ repo_dir="$(git rev-parse --show-toplevel)"
 sh "${repo_dir}/script/wait-db.sh"
 
 go run "${repo_dir}/cmd/migration/main.go" -o up -t schema
+go run "${repo_dir}/cmd/migration/main.go" -o down -t record
 go run "${repo_dir}/cmd/migration/main.go" -o up -t record
 
 go test "${repo_dir}/internal/..."
